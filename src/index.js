edced5290cdc8c754549f16c7ca28c4c7a0d645f
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-08-05 21:24:12 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+// import { Provider } from "react-redux";
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <Provider state={state}> */}
     <App />
+    {/* </Provider> */}
   </React.StrictMode>
 );
